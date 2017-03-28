e6925dccc7bd07c6bec2eead021bd4528b85aec3
calvinMcM <calvin.r.mcm@gmail.com>
2017-03-28 09:50:52 -0600

    Worked in a primitive html doc for us to use.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fs.readFile('config.json',function(err,data){
 function run(config){
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile('views/index.html',{root: __dirname})
 })
 
 app.listen(config.port, function () {
