fcaa3704e811f9d26c22bdb67038a93e443b13ea
calvinMcM <calvin.r.mcm@gmail.com>
2017-03-30 15:14:43 -0600

    Added some more logic for the login server.

---
+++ loginServer/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+// Set up public routes
+app.use(express.static('public'));
+
+// Mongo DB
+
+// File System
+const fs = require('fs');
+
+// Read configuration file before running.
+fs.readFile('config.json',function(err,data){
+    if(err){
+        console.log("Unable to read server configuration file!");
+    }
+    else{
+        data = JSON.parse(data);
+        // console.log("Found data:",data);
+        run(data);
+    }
+})
+
+
+function run(config){
+
+app.get('/login', function (req, res) {
+    // Verify user credentials and assign to a slave server.
+})
+
+app.listen(config.port, function () {
+  console.log('Example app listening on port',config.port + '!')
+})
+
+}
