83799154ca3cd961414f100def63b488aa223c85
Ubuntu <ubuntu@ip-172-31-29-115.us-west-2.compute.internal>
2017-04-13 19:01:50 +0000

    ok, server returns id and url on client login, client then redirects to google as a placeholder

--- loginServer/public/javascripts/google.js
+++ loginServer/public/javascripts/google.js
@@ -1,8 +1,11 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   var userInfo = {"username": profile.getEmail(), "userId": profile.getId()}
-  console.log(userInfo)
+  console.log("data to be sent", userInfo)
   $.post('googleCallback', userInfo, function(data) {
     console.log("Data sent")
+    console.log("recieved data", data)
+    //set cookie here!!! data.id
+    window.location = data.url
   })
 }
