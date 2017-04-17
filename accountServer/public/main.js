81b1c08f28d15aa0c59e1f483cf73678c48100ce
calvinMcM <calvin.r.mcm@gmail.com>
2017-04-17 08:58:48 -0600

    Story update and read display is now built out...

--- accountServer/public/main.js
+++ accountServer/public/main.js
@@ -8,9 +8,46 @@ $(document).ready(function(){
         // window.location.href = "http://34.208.82.175:3000";
     }
 
-    function populateStories(stories){
+    function buildStory(){
 
+        let story = {
+            title: "Jack And Jill",
+            author: "Jane",
+            story:[
+                "Jack and Jill went up a hill",
+                "to fetch a pail of water",
+                "Jack fell down",
+                "And broke his crown",
+                "And Jill came tumbling after!"
+            ]
+        }
+
+        storyContainer.empty();
+        var storyBox = $('<div>');
+        storyBox.addClass("story-box");
+        storyBox.addClass("padded");
+
+        var title = $("<h1>"+story.title+"</h1>");
+        storyBox.append(title);
+
+        for(var s of story.story){
+            storyBox.append($("<div class='darkField storyLine'>"+s+"<h4></h4></div>"))
+        }
+
+        var textArea = $("<h1>Next Line:</h1><textarea id='storyText' style='width: 100%' rows='3' class='darkField' placeholder='All of the sudden...'></textarea>");
+        storyBox.append(textArea);
+
+        var saveButton = $('<button>Save</button>')
+        saveButton.addClass('btn btn-green')
+        saveButton.on('click',function(){
+            story.story.push(textArea.val())
+            console.log("Now Saving Contribution Story:",data);
+        });
+        storyBox.append(saveButton);
+
+        storyContainer.append(storyBox);
     }
+    $(".storyDescriptor").on('click',buildStory);
 
     function populateDescriptors(descriptors){
         for(var d of descriptors){
@@ -41,7 +78,7 @@ $(document).ready(function(){
         saveButton.on('click',function(){
             let titleText = $('#storyTitle').val();
             let storyText = $('#storyText').val();
-            let data = {title:titleText, story:storyText, author:"Insert ID here"}
+            let data = {title:titleText, story:[storyText], author:"Insert ID here"}
             console.log("Now Saving Story:",data);
         });
         storyBox.append(saveButton);
